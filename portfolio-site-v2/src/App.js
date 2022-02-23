0dffadccda1349115e7c117f682a3f9f467803fe
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-23 18:46:06 -0500

    wasn't thinking the react way and I restarted

--- portfolio-site-v2/src/App.js
+++ portfolio-site-v2/src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
 
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Technologies from "./components/Technologies";
-import Contact from "./components/Contact";
-
 const App = () => {
   return (
     <>
-      <NavBar />
-      <div style={{ margin: '20% 5%'}}>
-        <Hero />
-        <About />
-        <Projects />
-        <Technologies />
-        <Contact />
-      </div>
+        hello world
     </>
   );
 };
