845227393118e8a0319c8ca6f98d65ab8f4d2525
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-23 16:45:54 -0500

    inital setup of section components - a blank canvas

--- portfolio-site-v2/src/App.js
+++ portfolio-site-v2/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Technologies from './components/Technologies';
+import Contact from './components/Contact';
+
+
 const App = () => {
 
   return (
-    <h1>Hello World!</h1>
+    <>
+        <NavBar />
+        <Hero />
+        <About />
+        <Projects />
+        <Technologies />
+        <Contact />
+    </>
   );
 }
 
