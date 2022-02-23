b9a4ac671b686762b72e61b1e822b84d9930b9b5
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-23 18:10:30 -0500

    added a div with margin for non nav pages default

--- portfolio-site-v2/src/App.js
+++ portfolio-site-v2/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import NavBar from './components/NavBar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Technologies from './components/Technologies';
-import Contact from './components/Contact';
-
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Technologies from "./components/Technologies";
+import Contact from "./components/Contact";
 
 const App = () => {
-
   return (
     <>
-        <NavBar />
+      <NavBar />
+      <div style={{ margin: '20% 5%'}}>
         <Hero />
         <About />
         <Projects />
         <Technologies />
         <Contact />
+      </div>
     </>
   );
-}
+};
 
 export default App;
