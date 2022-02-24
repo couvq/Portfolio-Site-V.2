04aab35a03dd2169aecee009bfb293a6d9b6934e
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 15:27:09 -0500

    more styling, not responsive yet

--- portfolio-site-v2/src/components/Hero/Hero.js
+++ portfolio-site-v2/src/components/Hero/Hero.js
@@ -1,5 +1,8 @@
-import { Container } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 import React from "react";
+
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 import HeroText from "./HeroText";
 import HeroButtons from "./HeroButtons";
@@ -7,12 +10,21 @@ import Illustration from "./Illustration";
 
 const Hero = () => {
   return (
-    <Container sx={{ margin: "5rem 2.5rem", width: '100vw', height: '100vh', marginTop: '10rem' }}>
-      <div style={{ position: 'absolute'}}>
-        <HeroText />
-        <HeroButtons />
-      </div>
-      <Illustration />
+    <Container
+      sx={{
+        margin: "5rem 2.5rem",
+        width: "100vw",
+        height: "100vh",
+        marginTop: "10rem",
+      }}
+    >
+      <Stack direction="row" spacing={100}>
+        <div style={{ position: "absolute" }}>
+          <HeroText />
+          <HeroButtons />
+        </div>
+        <Illustration />
+      </Stack>
     </Container>
   );
 };
