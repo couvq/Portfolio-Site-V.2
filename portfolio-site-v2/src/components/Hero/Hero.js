e740f50625bd20e64b2b91435e0d0a183a415866
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 15:25:26 -0500

    more styling of hero

--- portfolio-site-v2/src/components/Hero/Hero.js
+++ portfolio-site-v2/src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ import Illustration from "./Illustration";
 
 const Hero = () => {
   return (
-    <Container sx={{ margin: "5rem 2.5rem" }}>
+    <Container sx={{ margin: "5rem 2.5rem", width: '100vw', height: '100vh', marginTop: '10rem' }}>
       <div style={{ position: 'absolute'}}>
         <HeroText />
         <HeroButtons />
