67fbb52bb2b1804a4e028b31a67f7e4f26c064a1
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 15:40:13 -0500

    HeroButtons are now linked

--- portfolio-site-v2/src/components/Hero/HeroButtons.js
+++ portfolio-site-v2/src/components/Hero/HeroButtons.js
@@ -6,7 +6,7 @@ const HeroButtons = () => {
 
   return (
     <Stack direction="row" spacing={2} sx={{ marginTop: '1.5rem'}}>
-        <Button variant="contained" size='large' sx={{ color: '#fff'}}>See My Work</Button>
+        <Button variant="contained" size='large' sx={{ color: '#fff'}} href="#Projects">See My Work</Button>
         <Button variant="outlined" size='large' href="#About">About Me</Button>
     </Stack>
   );
