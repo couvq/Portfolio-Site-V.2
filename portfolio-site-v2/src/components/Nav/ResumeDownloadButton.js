8343186f2149765277ef53d58de6927b43096ac5
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 11:29:21 -0500

    download resume button now works

--- portfolio-site-v2/src/components/Nav/ResumeDownloadButton.js
+++ portfolio-site-v2/src/components/Nav/ResumeDownloadButton.js
@@ -1,14 +1,17 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button } from "@mui/material";
+import React from "react";
 
-import CloudDownloadSharpIcon from '@mui/icons-material/CloudDownloadSharp';
+import CloudDownloadSharpIcon from "@mui/icons-material/CloudDownloadSharp";
 
 const ResumeDownloadButton = () => {
-
-
   return (
-    <Button variant='contained' endIcon={<CloudDownloadSharpIcon />}>Download Resume</Button>
+    
+      <Button variant="contained" endIcon={<CloudDownloadSharpIcon />}>
+      <a href="/QuentinCouvelaireResume.pdf" style={{ textDecoration: 'none', color: 'white'}} download>
+        Download Resume
+      </a>
+      </Button>
   );
-}
+};
 
 export default ResumeDownloadButton;
