818506f8386d4131339c84d16ddb0066d39a111e
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 11:18:32 -0500

    responsiveness, navlinks now dissappear on sm and xs screens

--- portfolio-site-v2/src/components/Nav/NavLinks.js
+++ portfolio-site-v2/src/components/Nav/NavLinks.js
@@ -7,7 +7,17 @@ const NavLinks = () => {
 
 
   return (
-    <Stack direction="row" spacing={3}>
+    <Stack
+   direction="row" 
+   spacing={3}
+   sx= {{
+     display: {
+       xs: 'none',
+       sm: 'none',
+       md: 'flex'
+     }
+   }}
+   >
         <Button href='#'>About</Button>
         <Button href='#'>Projects</Button>
         <Button href='#'>Technologies</Button>
