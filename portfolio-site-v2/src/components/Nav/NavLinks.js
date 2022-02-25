5ec9b6779b5b7cad1d7cb5022afcf679c23295c9
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-24 20:13:38 -0500

    now can switch to blank navlinks when on french mode, getting close

--- portfolio-site-v2/src/components/Nav/NavLinks.js
+++ portfolio-site-v2/src/components/Nav/NavLinks.js
@@ -1,17 +1,34 @@
 import { Stack, Button } from '@mui/material';
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
+const data = {
+  En : ["About", "Projects", "Technologies", "Contact"],
+  Fr : ["", "", "", ""]
+}
+
+// console.log(data.En[0]); accesses "About"
 
 const NavLinks = ({ language }) => {
 
-  const changeLanguage = () => {
-    console.log('the language changed in NavLinks Component: ' + language);
+
+
+  // need to access four things (array? yes)
+  const [text, changeText] = useState([data.En[0], data.En[1], data.En[2], data.En[3]]);
+
+  // if language is english('En'), set to english. else set to french('Fr').
+  const changeTextOnLanguageChange = () => {
+    if (language === 'En') {
+      // change to english array
+      changeText([data.En[0], data.En[1], data.En[2], data.En[3]]);
+      return;
+    }
+    changeText([data.Fr[0], data.Fr[1], data.Fr[2], data.Fr[3]]);
   }
 
   // change textdata if language changes
 useEffect(() => {
-  changeLanguage();
+  changeTextOnLanguageChange();
 }, [language]);
 
   return (
@@ -26,10 +43,10 @@ useEffect(() => {
      }
    }}
    >
-        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#About'>About</Button>
-        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Projects'>Projects</Button>
-        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Technologies'>Technologies</Button>
-        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Contact'>Contact</Button>
+        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#About'>{text[0]}</Button>
+        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Projects'>{text[1]}</Button>
+        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Technologies'>{text[2]}</Button>
+        <Button sx={{ color: 'rgba(0,0,0,0.87)' }} href='#Contact'>{text[3]}</Button>
     </Stack>
   );
 }
