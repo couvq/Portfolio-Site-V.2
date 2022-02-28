bfbbd11dbc035e6476605fabac923164085d516a
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 11:06:03 -0500

    added about image

--- portfolio-site-v2/src/components/About/About.js
+++ portfolio-site-v2/src/components/About/About.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Container, Grid } from "@mui/material";
+import AboutImage from "./AboutImage";
 
 const About = () => {
-
-
   return (
-    <div id='About'>About</div>
+    <div id="About">
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item sm={12} md={6}>
+            <AboutImage />
+          </Grid>
+          <Grid item sm={12} md={6}>
+            {/* TODO: this is where our About me section text/ navlinks goes */}
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
   );
-}
+};
 
 export default About;
---
+++ portfolio-site-v2/src/components/About/AboutImage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { lightBlue } from '@mui/material/colors';
+
+const AboutImage = () => {
+
+
+  return (
+    <img 
+    src='/cosmoComposite19-20.jpg'
+    alt='Quentin Couvelaire About Me'
+    style={{ borderRadius: '50%', border: 'solid 3px #03a9f4', display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '100%'}}
+    />
+  );
+}
+
+export default AboutImage;
