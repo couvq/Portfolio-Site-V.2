44aa74cc28777daf8375cc1c84bec8ded0c505b6
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 17:41:50 -0500

    more skeleton code

---
+++ portfolio-site-v2/src/components/Projects/ProjectData.js
@@ -0,0 +1,1 @@
+// this simply holds our data 
--- portfolio-site-v2/src/components/Projects/ProjectList.js
+++ portfolio-site-v2/src/components/Projects/ProjectList.js
@@ -1,3 +1,4 @@
+// This component is responsible for grabbing our data from ProjectData and rendering ProjectCards from that data
 import React from 'react';
 
 const ProjectList = () => {
