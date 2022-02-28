e9c30866e533521da6bc23a7dba2c603bf5225a2
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 16:04:27 -0500

    edited grammar in hero section

--- portfolio-site-v2/src/components/Hero/HeroText.js
+++ portfolio-site-v2/src/components/Hero/HeroText.js
@@ -1,37 +1,41 @@
-import { Typography } from '@mui/material';
-import React from 'react';
-import { useState, useEffect } from 'react';
+import { Typography } from "@mui/material";
+import React from "react";
+import { useState, useEffect } from "react";
 
 const data = {
-  En: ["Hi! I'm Quentin", "I am a Frontend Developer passionate about creating stunning experiences for my users"],
-  Fr: ["Bonjour! Je m'appele Quentin", "Je suis un développeur frontend passionné par la création d'expériences époustouflantes pour mes utilisateurs"]
-}
+  En: [
+    "Hi! I'm Quentin.",
+    "I'm a Frontend Developer passionate about creating stunning experiences for my users.",
+  ],
+  Fr: [
+    "Bonjour! Je m'appele Quentin.",
+    "Je suis un développeur frontend passionné par la création d'expériences époustouflantes pour mes utilisateurs.",
+  ],
+};
 
 const HeroText = ({ language }) => {
-
   const [text, changeText] = useState([data.En[0], data.En[1]]); // english initially
 
   const changeTextOnLanguageChange = () => {
-    if(language === 'En') {
+    if (language === "En") {
       // set text to english text
       changeText([data.En[0], data.En[1]]);
       return; // don't continue
     }
     changeText([data.Fr[0], data.Fr[1]]);
-  }
+  };
 
   // change textdata if language changes
-useEffect(() => {
-  changeTextOnLanguageChange();
-}, [language]);
-
+  useEffect(() => {
+    changeTextOnLanguageChange();
+  }, [language]);
 
   return (
     <>
-        <Typography variant='h1'>{text[0]}</Typography>
-        <Typography variant='h6'>{text[1]}</Typography>
+      <Typography variant="h1">{text[0]}</Typography>
+      <Typography variant="h6">{text[1]}</Typography>
     </>
   );
-}
+};
 
 export default HeroText;
