47b267aa622339800184933bd83bdb692fe77d3d
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 15:15:37 -0500

    weird styling issues are plagueing this project

--- portfolio-site-v2/src/components/About/About.js
+++ portfolio-site-v2/src/components/About/About.js
@@ -1,27 +1,39 @@
 import { Grid } from "@mui/material";
 import React from "react";
 
-import AboutImage from './AboutImage';
-import AboutText from './AboutText';
+import AboutImage from "./AboutImage";
+import AboutText from "./AboutText";
 
 const About = () => {
   return (
-    <div 
-    id="About"
-    style={{
-      width: "100%",
-      height: 'auto'
-    }}
+    <div
+      id="About"
+      style={{
+        width: "100%",
+        height: "auto",
+        margin: '2.5rem 5rem',
+        marginTop: '2rem',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
     >
-    <Grid container spacing={5}>
-      <Grid item sm={12} md={4}>
-        <AboutImage />
+      <Grid
+        container
+        spacing={5}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid item xs={12} md={4}>
+          <AboutImage />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <AboutText />
+        </Grid>
       </Grid>
-      <Grid item sm={12} md={8}>
-        <AboutText />
-      </Grid>
-    </Grid>
-      About
     </div>
   );
 };
