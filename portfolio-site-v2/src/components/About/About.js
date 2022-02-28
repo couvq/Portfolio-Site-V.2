6e6eac34d17c0739ba2966d3c41066f6be438dd0
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 11:32:22 -0500

    more centering styles to about.js

--- portfolio-site-v2/src/components/About/About.js
+++ portfolio-site-v2/src/components/About/About.js
@@ -7,8 +7,8 @@ const About = () => {
   return (
     <div id="About" style={{width: '100vw', height: '100vh'}}>
       <Container>
-        <Grid container spacing={6} alignItems="center" justifyContent="center">
-          <Grid item sm={12} md={6}>
+        <Grid container spacing={6} justify="center" alignItems="center" justifyContent="center" margin="5rem 2.5rem">
+          <Grid item sm={12} md={6} >
             <AboutImage />
           </Grid>
           <Grid item sm={12} md={6}>
