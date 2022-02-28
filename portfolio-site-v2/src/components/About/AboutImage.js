403447bf0036bf081322d67d13eea7bffc819bff
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 15:50:25 -0500

    made border around image 1px larger

--- portfolio-site-v2/src/components/About/AboutImage.js
+++ portfolio-site-v2/src/components/About/AboutImage.js
@@ -7,7 +7,7 @@ const AboutImage = () => {
     <img 
     src='/cosmoComposite19-20.jpg'
     alt='Quentin Couvelaire About Me'
-    style={{ borderRadius: '50%', border: 'solid 3px #03a9f4', maxWidth: '100%', maxHeight: '100%', objectFit: 'cover'}}
+    style={{ borderRadius: '50%', border: 'solid 4px #03a9f4', maxWidth: '100%', maxHeight: '100%', objectFit: 'cover'}}
     />
   );
 }
