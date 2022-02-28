725a786b790fab646941e3cec738cd4023d6fbf7
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-28 12:46:36 -0500

    changed grid arrangement

--- portfolio-site-v2/src/components/About/About.js
+++ portfolio-site-v2/src/components/About/About.js
@@ -9,12 +9,12 @@ const About = () => {
     <div id="About" style={{ width: "100vw", height: "100vh" }}>
       <Container>
         <Grid container spacing={6} justify="center" margin="5rem 2.5rem">
-          <Grid item sm={12} md={6} justify="center">
+          <Grid item sm={12} md={4} justify="center">
             <Box display="flex" justifyContent="center" alignItems="center">
               <AboutImage />
             </Box>
           </Grid>
-          <Grid item sm={12} md={6}>
+          <Grid item sm={12} md={8}>
             <AboutText />
           </Grid>
         </Grid>
