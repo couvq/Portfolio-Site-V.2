5bcc0d4a865a62e81ee5076c2b0eb1af7a8f497b
couvelaireq <quentin.couvelaire@hope.edu>
2022-03-02 14:27:04 -0500

    variant now changes for active buttons, text white comes next

--- portfolio-site-v2/src/components/Projects/Projects.js
+++ portfolio-site-v2/src/components/Projects/Projects.js
@@ -1,30 +1,61 @@
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react";
 
-import { Button, Stack, Typography } from '@mui/material';
-import React from 'react';
+import { useState } from "react";
 
 const Projects = () => {
+  const [active, setActive] = useState("All"); // either 'All', 'Personal', 'Work', or 'Research'
 
   return (
-    <div id="Projects"
-    style={{
-      padding: '2.5rem 5rem',
-      width: '100vw',
-      height: '100vh',
-      marginTop: '5rem'
-    }}
+    <div
+      id="Projects"
+      style={{
+        padding: "2.5rem 5rem",
+        width: "100vw",
+        height: "100vh",
+        marginTop: "5rem",
+      }}
     >
-    <Typography variant="h2">My Projects</Typography>
-    <br />
-    <Typography variant='h5'>Filter By: </Typography>
-    <Stack direction="row" spacing={2}>
-      <Button variant="contained">All</Button>
-      <Button>Frontend</Button>
-      <Button>Backend</Button>
-      <Button>Research</Button>
-    </Stack>
-    {/* TODO: This is where our project cards will be rendered*/}
+      <Typography variant="h2">My Projects</Typography>
+      <br />
+      <Typography variant="h5">Filter By: </Typography>
+      <Stack direction="row" spacing={2}>
+        <Button
+          variant={active === "All" ? "contained" : ""}
+          onClick={() => {
+            setActive("All");
+          }}
+        >
+          All
+        </Button>
+        <Button
+          variant={active === "Personal" ? "contained" : ""}
+          onClick={() => {
+            setActive("Personal");
+          }}
+        >
+          Personal
+        </Button>
+        <Button
+          variant={active === "Work" ? "contained" : ""}
+          onClick={() => {
+            setActive("Work");
+          }}
+        >
+          Work
+        </Button>
+        <Button
+          variant={active === "Research" ? "contained" : ""}
+          onClick={() => {
+            setActive("Research");
+          }}
+        >
+          Research
+        </Button>
+      </Stack>
+      {/* TODO: This is where our project cards will be rendered*/}
     </div>
   );
-}
+};
 
 export default Projects;
